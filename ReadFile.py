a5096ce6579fc942250464f0ad287bc2cc4fd637
zhefan <amandafan@AmandadeMacBook-Air.local>
2017-12-02 19:07:32 -0800

    TFIDF function and News data read
    Implemented the get_news function in ReadFile.py. It return a list of
    news within the given time
    Implemented the TFIDF.py class. Sample usage is given in the file.

--- ReadFile.py
+++ ReadFile.py
@@ -1,15 +1,25 @@
 import sys
+import csv
 
 class ReadFile:
 
-  def __init__(self, date):
-    self.news = self.get_news(date);
-    self.price = self.get_price(date);
+    def __init__(self):
+        # RedditNews.csv contains news from 2008-06-08 to 2016-07-01
+        self.news = []
+        self.price = []
+        with open('RedditNews.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                self.news.append(row)
 
-  # Read Files and get all the news published on the given date
-  def get_news(self, date):
-    return "";
+    # Read Files and get all the news published on the given date
+    def get_news(self, date):
+        news_on_date = []
+        for entry in self.news:
+            if date in entry["Date"]:
+                news_on_date.append(entry["News"])
+        return news_on_date
 
-  # Read Files and get the closing price of the given date
-  def get_price(self, date):
-    return 0;
+    # Read Files and get the closing price of the given date
+    def get_price(self, date):
+        return 0;
---
+++ TFIDF.py
@@ -0,0 +1,43 @@
+from ReadFile import ReadFile
+import math
+
+
+class TFIDF:
+
+    '''
+    This class takes a string term and a string formated time,
+    then find all the related news within the time range.
+    Finally it calculate the TF-IDF score for that term
+    Sample usage:
+    tfidf = TFIDF("Google", "2016-06")
+    score = tfidf.findTFIDF();
+    print(score)
+    '''
+    def __init__(self, term, time):
+        file = ReadFile();
+        self.newsList = file.get_news(time);
+        self.t = term
+        # print(self.newsList)
+        # print(self.newsList.__len__())
+
+    # TF(t, d) = (Number of times term t appears in document d) / (Total number of terms in the document).
+    # Find the sum of all TF for d in newsList
+    def findTFIDF (self):
+        tfscore = 0.0
+        count = 0
+        for d in self.newsList:
+            if self.t in d:
+                list_of_terms = d.split(' ')
+                tfscore += d.count(self.t) / list_of_terms.__len__()
+                count += 1
+                # print(tfscore)
+                # print(count)
+        idfscore = math.log(self.newsList.__len__() / count)
+        # print(idfscore)
+        return tfscore * idfscore
+
+    # IDF(t, D) = log_e(Total number of documents in D / Number of documents with term t in it). [9]
+    def findIDF (self):
+
+        return 0
+
